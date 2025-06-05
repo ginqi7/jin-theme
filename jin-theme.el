6792dd1e41e04f41938771dbd32ecc570fe6ce03
Qiqi Jin <ginqi7@gmail.com>
2025-06-05 19:26:57 +0800

    feat: assign a unique name to each color

--- jin-theme.el
+++ jin-theme.el
@@ -26,80 +26,71 @@
 
 (deftheme jin "A personal light theme.")
 
-(custom-theme-set-faces
- 'jin
- `(default
-   ((((type tty))
-     :background "#f8f8f8")
-    (((type graphic))
-     :background "#fdf6e3"
-     :foreground "black")))
- `(hl-line
-   ((t (:box (:line-width (-1 . -2) :color "#edccb7" :style nil)))))
- `(cursor ((t (:background "#6c71c4"))))
- '(region ((t (:background "#edccb7"))))
- '(fringe ((t)))
- ;; font lock
- `(font-lock-comment-face
-   ((t
-     (:foreground ,"#c4a599" :weight normal :italic t :variable-pitch t))))
- `(font-lock-doc-face ((t (:inherit ,font-lock-comment-face))))
- `(font-lock-warning-face ((t (:foreground "red"))))
- `(font-lock-string-face ((t (:background "#FFEE99"))))
- `(font-lock-function-name-face ((t (:foreground "Blue"))))
- `(font-lock-keyword-face ((t (:foreground "#6c71c4"))))
- `(font-lock-constant-face ((t (:foreground "#381a8a"))))
- `(font-lock-builtin-face ((t ())))
- `(font-lock-variable-name-face ((t ())))
- `(font-lock-type-face ((t (:foreground "#b58900"))))
- `(header-line ((t (:foreground "#b58900"))))
- `(font-lock-preprocessor-face
-   ((t (:inherit font-lock-constant-face))))
- ;; meow
- '(meow-keypad-indicator
-   ((t (:foreground "#801717" :background "#FF6666"))))
- '(meow-insert-indicator
-   ((t (:foreground "#309030" :background "#AAE9A0"))))
- '(meow-normal-indicator
-   ((t (:foreground "#6F5033" :background "#FFEE99"))))
- '(meow-motion-indicator
-   ((t (:foreground "#505090" :background "#AACCEE"))))
- '(meow-keypad-cursor ((t (:background "#7c2902"))))
- '(meow-insert-cursor ((t (:background "#014f39"))))
- '(meow-normal-cursor ((t (:background "#dc322f"))))
- '(meow-motion-cursor ((t (:background "#035063"))))
- '(secondary-selection ((t (:background "#fed48b"))))
- ;; dired
- '(dired-directory ((t (:inherit font-lock-keyword-face))))
- `(parenthesis ((t (:foreground "#909090"))))
- '(highlight-symbol-face ((t ())))
- ;; Rime
- '(rime-preedit-face ((t (:inverse-video nil :underline t))))
- '(rime-cursor-face ((t (:inherit font-lock-constant-face))))
- '(cider-fringe-good-face ((t (:foreground "#006666"))))
- `(line-number-current-line
-   ((((type graphic))
-     :box (:line-width (-1 . -2) :color "#edccb7" :style nil)
-     :bold t
-     :foreground "#404040"
-     :inherit default)
-    (t (:bold t :inherit default :background "#ffffff"))))
- '(window-divider ((t (:foreground "grey60"))))
- '(vertical-border ((t (:foreground "grey20"))))
- ;; org mode
- '(org-block ((t (                      ;:background "#efffef"
-                  ;;      :box (:line-width (-1 . -2) :color "#edccb7" :style nil)
-                  :extend t))))
- '(org-table ((t (:foreground "grey40"))))
- '(org-level-1 ((t (:bold t))))
- '(org-level-2 ((t (:bold t))))
- '(org-level-3 ((t (:bold t))))
- '(org-level-4 ((t (:bold t))))
- '(org-level-5 ((t (:bold t))))
- '(org-level-6 ((t (:bold t))))
- '(org-level-7 ((t (:bold t))))
- ;; diff
- '(diff-changed ((t (:background "#fed48b")))))
+(let ((main-bg "#fdf6e3")
+      (main-fg "black")
+      (region-bg "#edccb7")
+      (keyword-fg "#5b21b6")
+      (string-bg "#faece1")
+      (constant-fg "#381a8a")
+      (function-fg "#9333ea")
+      (variable-fg "#0f766e")
+      (type-fg "#16a34a")
+      (comment-fg "#c4a599")
+      (error-fg "#db2777")
+      (warning-fg "#eab308")
+      (secondary-selection-bg "#c7d2fe")
+      (cursor-bg "#dc322f")
+      (diff))
+
+  (custom-theme-set-faces
+   'jin
+   `(default
+     ((((type graphic))
+       :background ,main-bg
+       :foreground ,main-fg)))
+   `(hl-line
+     ((t (:box (:line-width (-1 . -2) :color ,region-bg :style nil)))))
+   `(region ((t (:background ,region-bg))))
+   '(fringe ((t)))
+   ;; font lock
+   `(font-lock-comment-face
+     ((t
+       (:foreground ,comment-fg :weight normal :italic t :variable-pitch t))))
+   `(font-lock-doc-face ((t (:inherit ,font-lock-comment-face))))
+   `(font-lock-warning-face ((t (:foreground ,warning-fg))))
+   `(font-lock-string-face ((t (:background ,string-bg))))
+   `(font-lock-function-name-face ((t (:foreground ,function-fg))))
+   `(font-lock-keyword-face ((t (:foreground ,keyword-fg))))
+   `(font-lock-constant-face ((t (:foreground ,constant-fg))))
+   `(font-lock-builtin-face ((t ())))
+   `(font-lock-variable-name-face ((t (:foreground ,variable-fg))))
+   `(font-lock-type-face ((t (:foreground ,type-fg))))
+   `(font-lock-preprocessor-face
+     ((t (:inherit font-lock-constant-face))))
+   ;; meow
+   `(meow-normal-cursor ((t (:background ,cursor-bg))))
+   `(secondary-selection ((t (:background ,secondary-selection-bg))))
+   ;; dired
+   '(dired-directory ((t (:inherit font-lock-keyword-face))))
+   '(highlight-symbol-face ((t ())))
+   ;; Rime
+   '(rime-preedit-face ((t (:inverse-video nil :underline t))))
+   '(rime-cursor-face ((t (:inherit font-lock-constant-face))))
+   `(line-number-current-line
+     ((t
+       :box (:line-width (-1 . -2) :color ,region-bg :style nil)
+       :bold t
+       :foreground ,main-fg
+       :inherit default)))
+
+   ;; org mode
+   '(org-level-1 ((t (:bold t))))
+   '(org-level-2 ((t (:bold t))))
+   '(org-level-3 ((t (:bold t))))
+   '(org-level-4 ((t (:bold t))))
+   '(org-level-5 ((t (:bold t))))
+   '(org-level-6 ((t (:bold t))))
+   '(org-level-7 ((t (:bold t))))))
 
 (and load-file-name
      (boundp 'custom-theme-load-path)
